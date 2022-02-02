6880dd733070feb280c9ca156f411eb99e9670e4
FLvH501 <ferdinandvonholstein@protonmail.com>
2022-02-02 11:24:49 +0000

    First attempt at second piece of code

--- scripts.js
+++ scripts.js
@@ -2,3 +2,19 @@ function computerPlay() {
     const compOptions = ["rock", "paper", "scissors"];
     return compOptions[Math.floor(Math.random() * compOptions.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    const playerOptions = ["rock", "paper", "scissors"];
+
+    if ((playerSelection == "rock" && computerPlay(computerSelection) == "scissors") || (playerSelection == "scissors" && computerPlay(computerSelection) == "paper") || (playerSelection == "paper") && computerPlay(computerSelection) == "rock")) {
+    alert("You win!");
+} else if (playerSelection == "rock") && computerPlay(computerSelection) == "paper") || (playerSelection == "scissors") && computerPlay(computerSelection) == "rock" || (playerSelection == "paper") && computerPlay(computerSelection) == "scissors")) {
+    alert("You Lose.")
+} else(playerSelection != "rock", "paper", "scissors") {
+    alert("Entered invalid move.")
+}
+}
+
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
